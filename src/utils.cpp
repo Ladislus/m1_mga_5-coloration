264edb7576a702ee4e672ebcf694c4b246c4d78e
Ladislus <walcak.ladislas@gmail.com>
2020-11-16 02:00:58 +0100

    [MOD] Change data structure Removed Edge class Vertex : neighbors changed from vector to unordered_map Graph : combined the vector of edges and the vector of vertices inside one unordered_map TODO : Graph::solve()

--- src/utils.cpp
+++
@@ -1,7 +0,0 @@
-#include "utils.hpp"
-
-std::vector<int> range(int start, int to) {
-    std::vector<int> res;
-    for (int i = start; i < to; ++i) { res.push_back(i); }
-    return res;
-}
--- src/vertex.cpp
+++ src/vertex.cpp
@@ -1,13 +1,27 @@
 #include "vertex.hpp"
 
-Vertex::Vertex(const std::string& identifier) : _identifier(identifier), _color(-1) {}
+Vertex::Vertex(const std::string &identifier): _identifier(identifier), _color(-1), _neighbors() {}
 
-int Vertex::color() const { return this->_color; }
+const std::string& Vertex::identifier() const { return this->_identifier; }
 
-int& Vertex::color() { return this->_color; }
+int& Vertex::color() { return _color; }
 
-std::ostream& operator<<(std::ostream& out, const Vertex& e) {
-    return out << e._identifier << "(" << e._color << ")";
+nvector Vertex::neighbors() const {
+    nvector returned;
+    for (const auto& neighbor : this->_neighbors) returned.push_back(neighbor.second);
+    return returned;
 }
 
-bool Vertex::operator==(const Vertex& other) { return this->_identifier == other._identifier; }
+void Vertex::addNeighbor(Vertex* vertex) {
+    const auto& it = this->_neighbors.find(vertex->identifier());
+    if (it == this->_neighbors.end()) {
+        this->_neighbors.insert(std::pair(vertex->identifier(), vertex));
+        vertex->addNeighbor(this);
+    }
+}
+
+std::ostream& operator<<(std::ostream& out, const Vertex& e) {
+    out << e._identifier << "(" << e._color << ")" << std::endl << "\t";
+    for (const auto& neighbor : e._neighbors) out << neighbor.second->identifier() << " ";
+    return out << std::endl;
+}
