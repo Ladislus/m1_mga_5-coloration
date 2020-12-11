f1e265966a9b98b04f338c78642b38093265a9cd
Ladislus <walcak.ladislas@gmail.com>
2020-12-11 02:48:13 +0100

    [ADD] Added comments for Vertex

--- include/vertex.hpp
+++ include/vertex.hpp
@@ -7,29 +7,88 @@
 #include <algorithm>
 #include <stack>
 
+/**
+ * Enum to represent all 5 colors available for coloring
+ */
 enum Color { vide = -1, blue = 0, red = 1, green = 2, white = 3, black = 4 };
 
+/**
+ * Class that represent a Vertex inside the graph
+ * These vertices are created with an adjacency list
+ */
 class Vertex {
 
     private:
+        /**
+         * The identifier of the Vertex
+         */
         std::string _identifier;
+        /**
+         * The color of the vertex (default: vide)
+         */
         Color _color;
+        /**
+         * A list of pointers to all the neighbors of the Vertex
+         */
         std::vector<Vertex*> _neighbors;
+        /**
+         * (x, y) position for graphic output (default: 0)
+         */
         int _x, _y;
 
     public:
         explicit Vertex(const std::string& identifier, const Color& color = vide);
 
+        /**
+         * Method to get the identifier
+         * @return the identifier of the vertex (const reference to prevent copy)
+         */
         const std::string& identifier() const;
+        /**
+         * Getter / Setter of the color of the vertex
+         * @return A reference to the color of the Vertex
+         */
         Color& color();
+        /**
+         * Method to get the string representation of the color of the Vertex
+         * @return The color of the Vertex
+         */
         std::string printableColor() const;
+        /**
+         * Method to get the list a neighbors of the Vertex (const reference to prevent copy)
+         * @return The list of neighbors
+         */
         const std::vector<Vertex*>& neighbors() const;
+        /**
+         * Getter / Setter of the x coordinate
+         * @return A reference to the x coordinate
+         */
         int& x();
+        /**
+        * Getter / Setter of the y coordinate
+        * @return A reference to the y coordinate
+        */
         int& y();
 
+        /**
+         * Method to add a Vertex to the list of neighbors
+         * @param vertex A pointer to the Vertex to add
+         */
         void addNeighbor(Vertex* vertex);
 
+        /**
+         * Method to check whether or not the current Vertex can reach the target Vertex on
+         * the subgraph that contains all the vertices that have the same color as ourself or target,
+         * and free one color by flipping all the colors of this subgraph if they can't reach one another
+         * @param target The Vertex that we try to reach
+         * @param ignored The Vertex that called the function (G' = G - x)
+         * @return false if they can reach one another, true if they can't and the colors were flipped
+         */
         bool flipIfUnreachable(Vertex* target, Vertex* ignored);
+        /**
+         * Method to colorize the current Vertex
+         * @param ignored A list of all the vertices that must be ignored
+         */
         void colorize(std::set<std::string> ignored);
 
         friend std::ostream& operator<<(std::ostream& out, const Vertex& e);
--- src/vertex.cpp
+++ src/vertex.cpp
@@ -25,13 +25,17 @@ std::string Vertex::printableColor() const {
 }
 
 void Vertex::addNeighbor(Vertex* vertex) {
+    // If vertex is already a neighbor, skip
     for (const auto& neighbor : this->_neighbors) if (neighbor->_identifier == vertex->_identifier) return;
+    // Else add vertex to the list of neighbors
     this->_neighbors.push_back(vertex);
 }
 
 bool Vertex::flipIfUnreachable(Vertex* target, Vertex* ignored) {
+    // Variable to store whether or not the target has been reached
     bool reached = false;
     std::set<Vertex*> seen;
+    // LIFO structure for depth search
     std::stack<Vertex*> todo;
     todo.push(this);
 
@@ -40,9 +44,12 @@ bool Vertex::flipIfUnreachable(Vertex* target, Vertex* ignored) {
         Vertex* v = todo.top();
         todo.pop();
         seen.insert(v);
+        // For all the neighbors of the current Vertex
         for (const auto& neighbor : v->neighbors()) {
+            // If the neighbor is the target, set reached to trye
             if (neighbor->identifier() == target->identifier())
                 reached = true;
+            // If the neighbor has not already been seen, is part of the correct subgraph, and is not ignored, add it to the todo list
             if (seen.find(neighbor) == seen.end() &&
                 neighbor->identifier() != ignored->identifier() &&
                 (neighbor->color() == target->color() || neighbor->color() == this->_color))
@@ -50,20 +57,25 @@ bool Vertex::flipIfUnreachable(Vertex* target, Vertex* ignored) {
         }
     }
 
+    // If the two vertices can reach one another, return false
     if (reached) return false;
 
+    // Else, set the caller's color to the color that is going to be free'd
     ignored->color() = this->_color;
 
+    // For each element in the depth search, swap the colors (block 3)
     for (const auto& v : seen)
         if (v->color() == target->color()) v->color() = this->color();
         else v->color() = target->color();
 
+    // Return true
     return true;
 }
 
 void Vertex::colorize(std::set<std::string> ignored) {
-
+    // Add itself to the ignored list for leaves vertices
     ignored.insert(this->_identifier);
+    // Recursive call for all leaves neighbors that are not already colored
     for (const auto& neighbor : this->_neighbors)
         if (ignored.find(neighbor->_identifier) == ignored.end() && neighbor->_color == vide)
             neighbor->colorize(ignored);
@@ -71,22 +83,30 @@ void Vertex::colorize(std::set<std::string> ignored) {
     std::set<int> availableColors;
     std::set<int> colors { 0, 1, 2, 3, 4 };
     std::set<int> neighborsColor;
+    // Get the colors of the neighbors
     for (const auto& neighbor : this->_neighbors)
         if (neighbor->_color != vide)
             neighborsColor.insert(neighbor->color());
 
+    // If all 5 colors are already used by the neighbors ...
     if (neighborsColor.size() == colors.size()) {
+        // ... Try block 6 with a and c ...
         if (!this->_neighbors.at(0)->flipIfUnreachable(this->_neighbors.at(2), this))
+            // ... If it failed, try block 6 with b and d ...
             if (!this->_neighbors.at(1)->flipIfUnreachable(this->_neighbors.at(3), this)) {
+                // ... If both failed, there is a problem, raise an error
                 std::cerr << "Error: Flipping does not resolve the problem (FLIP_INEFFECTIVE)" << std::endl;
                 std::cerr << "Debug: " << this->_identifier << std::endl;
                 exit(1);
             }
+    // If there are less than 5 colors used
     } else {
+        // Compute the complement of the available colors with the colors used
         std::set_difference(colors.begin(), colors.end(),
                             neighborsColor.begin(), neighborsColor.end(),
                             std::inserter(availableColors, availableColors.end()));
 
+        // Set the color of the Vertex to the first color available
         this->_color = static_cast<Color>(*availableColors.begin());
     }
 }
